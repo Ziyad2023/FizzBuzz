ee22b3fba2fd27ced6c125559a2fea53d56d8a84
Ziyad Wakil <ziyad.wakil@mrjaffesclass.com>
2022-10-19 11:44:41 -0700

    Day 1

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i = i +1){
+            System.out.println(i);
+        }
+        for (int i = 1; i <= 100; i = i +3) {
+            System.out.println("Fizz");
+        }
+    }
+}
